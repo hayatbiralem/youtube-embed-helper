a3acf691d197d928e5481b27b9e8114994386100
Ömür Yanıkoğlu <hayatbiralem@gmail.com>
2020-05-27 12:41:56 +0300

    API ready callback issue fixed

--- src/js/utils/loadAPI.js
+++ src/js/utils/loadAPI.js
@@ -1,18 +1,48 @@
-export default function loadAPI(cb) {
-  if (!document.querySelector('script[id=youtube-api]')) {
+let keyCallbacks = 'YouTubeEmbedHelper_API_Callbacks';
+let keyFetched = 'YouTubeEmbedHelper_API_Fetched';
+let keyLoaded = 'YouTubeEmbedHelper_API_Loaded';
 
-    // Listen ready
-    window.onYouTubeIframeAPIReady = function () {
-      cb && cb();
-    };
+window[keyCallbacks] = window[keyCallbacks] || [];
 
-    // Call the youtube api
-    let tag = document.createElement('script');
-    tag.id = 'youtube-api';
-    tag.src = 'https://www.youtube.com/iframe_api';
-    let firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  } else {
-    cb && cb();
+const addCallback = function(cb){
+  if(cb) {
+    window[keyCallbacks].push(cb);
   }
+};
+
+const runCallbacks = function(){
+  if(window[keyCallbacks].length > 0) {
+    window[keyCallbacks].forEach(function(cb){
+      cb();
+    });
+  }
+};
+
+export default function loadAPI(cb) {
+
+  if(window[keyLoaded]) {
+    cb && cb();
+  } else {
+    addCallback(cb);
+
+    if (!window[keyFetched]) {
+      window[keyFetched] = true;
+
+      // Listen ready
+      window.onYouTubeIframeAPIReady = function () {
+        window[keyLoaded] = true;
+        runCallbacks();
+      };
+
+      // Call the youtube api
+      let tag = document.createElement('script');
+      tag.id = 'youtube-api';
+      tag.src = 'https://www.youtube.com/iframe_api';
+      let firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
+
+
+  }
+
 }
